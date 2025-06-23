134322912dfde2d63ac9d0219268a9415219ea37
Louie Schiltz <Lfschiltz95@gmail.com>
2025-06-23 18:46:11 -0500

    Day 1

---
+++ 1DayExample3.py
@@ -0,0 +1,3 @@
+glass1 = "milk"
+glass2 = "juice"
+glass1,glass2 = glass2,glass1
---
+++ 1DayFinalProj.py
@@ -0,0 +1,13 @@
+#my version
+print("Welcome the Band Name Generator.")
+home_city = input("What's the name of the city you grew up in? ")
+print(home_city)
+pet_name = input("What's your pet's name? ")
+print(pet_name)
+print(f"Your band name could be {home_city} {pet_name}.")
+
+#Angela's version
+print("Welcome to the Band Name Generator.")
+city = input("Which city did you grew up in?\n")
+pet = input("What is the name of your pet?\n")
+print("Your band name could be: " + city + " " + pet)
